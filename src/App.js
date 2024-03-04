e414203fb4092861409e65dee8b5b406d179477f
경원 <121841669+deswaq1220@users.noreply.github.com>
2024-03-04 15:50:40 +0900

    멘토링

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css'
 
 function App() {
+  const item = [0,1,2,3,4,5,6,7,8]
+  const [text,setText] = useState(item)
+
+  const handleClick = (index) => {
+    let newItem = [...text] 
+    newItem[index] = 'x'
+    setText(newItem)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic Tac Toe</h1>
+      <div className="container">
+        <ul className="wrap">
+          <li onClick={() => handleClick(0)}>{text[0]}</li>  
+          <li onClick={() => handleClick(1)}>{text[1]}</li>
+          <li onClick={() => handleClick(2)}>{text[2]}</li>
+        </ul>
+        <ul className="wrap">
+          <li onClick={() => handleClick(3)}>{text[3]}</li>
+          <li onClick={() => handleClick(4)}>{text[4]}</li>
+          <li onClick={() => handleClick(5)}>{text[5]}</li>
+        </ul>
+        <ul className="wrap">
+          <li onClick={() => handleClick(6)}>{text[6]}</li>
+          <li onClick={() => handleClick(7)}>{text[7]}</li>
+          <li onClick={() => handleClick(8)}>{text[8]}</li>
+        </ul>
+      </div>
     </div>
   );
 }
